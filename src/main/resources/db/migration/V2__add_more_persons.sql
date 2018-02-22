7110057a91607fd7cb7f0d685dab7e793efef720
enmecsa <csaba.nemeth@ericsson.com>
2018-02-22 14:23:46 +0100

    Spring-boot starter project

---
+++ src/main/resources/db/migration/V2__add_more_persons.sql
@@ -0,0 +1,2 @@
+insert into PERSON (first_name, last_name) values ('Steve', 'Jobs');
+insert into PERSON (first_name, last_name) values ('Mark', 'Martin');
---
+++ src/test/java/com/ncs/sorsoldki/SorsoldkiApplicationTests.java
@@ -0,0 +1,31 @@
+package com.ncs.sorsoldki;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.rule.OutputCapture;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SorsoldkiApplicationTests
+{
+
+  @Rule
+  public OutputCapture outputCapture = new OutputCapture();
+
+  @Test
+  public void contextLoads()
+  {
+    SorsoldkiApplication.main(new String[] {});
+    String out = outputCapture.toString();
+    assertTrue("Wrong output: " + out,
+        out.contains("Started SorsoldkiApplication")
+            && out.contains("Migrating schema \"PUBLIC\" to version 1")
+    );
+  }
+
+}
